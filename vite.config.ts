7f86e443debd555364211dcc18374ec24536f34d
xiangliu <1651868595@qq.com>
2025-10-18 14:41:44 +0800

    fourteenth  commit

--- vite.config.ts
+++ vite.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     coverage: {
-      provider: "istanbul", // or 'v8',
-      reporter: ["text", "json", "html"],
+      provider: 'istanbul', // or 'v8',
+      reporter: ['text', 'json', 'html'],
       // include: ['src/**/*.ts', 'src/**/*.tsx'], // 指定你要测试的文件路径
       // exclude: ['src/*.ts', 'src/utils/**'],// 指定你要忽略的文件路径
     },
